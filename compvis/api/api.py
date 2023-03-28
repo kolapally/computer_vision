be457c8bdef866f6954c094e7c00f1818765b76b
saikalyan and kolapally <ksaikalyan96@gmail.com>
2023-03-28 12:00:16 +0200

    new version with updated params.py

--- compvis/api/api.py
+++ compvis/api/api.py
@@ -4,7 +4,7 @@ from starlette.responses import Response
 from compvis.preprocess.face_detect_single import face_detect_single
 from compvis.model.model import *
 from compvis.api.face_labeling import api_output
-from compvis.params import model_path
+from compvis.params import model_path,class_names,image_size
 import numpy as np
 import cv2
 app = FastAPI()
@@ -41,7 +41,7 @@ async def detect_faces(img: UploadFile=File(...)):
     #Office cast name lables
     class_names = ['Angela','Dwight','Jim','Kevin','Michael','Pam','unknown']
     #predict the input image
-    label, images = model_predict(app.state.model,cropped_img_path , class_names, target_size=(128,128))
+    label, images = model_predict(app.state.model,cropped_img_path , class_names=class_names, target_size=image_size)
     image_output = api_output(image,faces_coords,label)
     # Encoding and responding with the image
     im = cv2.imencode('.png', image_output)[1] # extension depends on which format is sent from Streamlit
