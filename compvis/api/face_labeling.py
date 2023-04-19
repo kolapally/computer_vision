c02262d4534dbc22e9ded80c1959ebdaf0bce9c1
sai kalyan Kolapally <68870960+kolapally@users.noreply.github.com>
2023-04-19 20:10:03 +0530

    Update face_labeling.py

--- compvis/api/face_labeling.py
+++ compvis/api/face_labeling.py
@@ -1,18 +1,25 @@
 import cv2
 color_map = {
-    "Angela": (0, 0, 255),
-    "Dwight": (0, 150, 150),
-    "Jim": (255, 0, 0),
-    "Kevin": (0, 0, 150),
-    "Michael": (200, 0, 0),
-    "Pam": (0, 255, 0),
-     "unknown": (128, 128, 128),
-#     "trailer": (0, 150, 150),
-#     "motorcycle": (0, 150, 0),
-#     "bus": (0, 0, 100),
+    "Angela": (255, 0, 0), # red
+    "Dwight": (0, 0, 255), #blue
+    "Jim": (0, 255, 0), #green
+    "Kevin": (255, 255, 0), #yellow
+    "Michael": (128, 0, 128), #purpule
+    "Pam": (255, 192, 203), # pink
+     "unknown": (255, 165, 0), #orange
 }
 def api_output(image,faces_coords,label):
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    """
+    Labels each detected face in an image with their predicted expression.
+
+    Parameters:
+    image (numpy.ndarray): The image with detected faces.
+    faces_coords (dict): A dictionary containing the coordinates of the detected faces.
+    label (dict): A dictionary containing the predicted expression labels for each detected face.
+
+    Returns:
+    numpy.ndarray: The input image with labeled faces.
+    """
     font = cv2.FONT_HERSHEY_SIMPLEX
     for face,coords in faces_coords.items():
             x, y, w, h = coords
