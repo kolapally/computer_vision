d55af1cefe91cb56952d1e1460c3fa45d7434c17
sai kalyan Kolapally <68870960+kolapally@users.noreply.github.com>
2023-04-19 19:53:58 +0530

    Update api_v2.py

--- compvis/api/api_v2.py
+++ compvis/api/api_v2.py
@@ -1,3 +1,4 @@
+#Importing the necessary modules and libraries for the web application
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import Response
@@ -10,6 +11,7 @@ from compvis.video.video_predict import *
 import numpy as np
 import cv2
 
+#Creating an instance of the FastAPI class and loading the pre-trained model
 app = FastAPI()
 app.state.model = model_load(model_path)
 # Allow all requests (optional, good for development purposes)
@@ -21,10 +23,17 @@ app.add_middleware(
      allow_headers=["*"],  # Allows all headers
  )
 
+#Defining the / endpoint. When accessed, this endpoint returns a JSON object with a message "status": "ok"
 @app.get("/")
 def index():
     return {"status": "ok"}
 
+'''
+Defining the /detect_image endpoint. This endpoint receives an image file in the request and returns the image with labeled faces as a response.
+The image is first read and decoded from bytes to a numpy array and then to a cv2 object.
+The face_detect_single function is called to detect faces in the image, and the model_predict function predicts the facial expressions of each detected face.
+The api_output function is called to label each detected face with their predicted expression, and the labeled image is returned as a response.
+'''
 @app.post("/detect_image")
 async def detect_image(img: UploadFile=File(...)):
     # Receiving and decoding the image
@@ -37,13 +46,6 @@ async def detect_image(img: UploadFile=File(...)):
     # Call face_detect_single function and get results
     cropped_img_path, faces_coords, image = face_detect_single(img,'jpg')
 
-    # Other elements of the pipeline will go here:
-    # Crop -> Predict -> get classified img
-    #load trained model
-    # model = model_load('/home/kolapally/code/kolapally/computer_vision/compvis/model/models/model.h5')
-    #Office cast name lables
-    #class_names = ['Angela','Dwight','Jim','Kevin','Michael','Pam','unknown']
-
     #predict the input image
     label, images = model_predict(app.state.model,cropped_img_path , class_names=class_names, target_size=image_size)
     image_output = api_output(image,faces_coords,label)
@@ -51,10 +53,16 @@ async def detect_image(img: UploadFile=File(...)):
     im = cv2.imencode('.png', image_output)[1] # extension depends on which format is sent from Streamlit
     return Response(content=im.tobytes(), media_type="image/png")
 
+'''
+Defining the /detect_video endpoint. This endpoint receives a video file in the request and returns the video with labeled frames as a response.
+The video is first saved locally, and then specific frames are selected to label using the frames_to_label_part_video function.
+The predict_video_frames function predicts the facial expressions of each selected frame.
+The make_labeled_videofunction is called to label each selected frame with their predicted expression, and the labeled video is returned as a response.
+The response includes the labeled video file and the necessary headers for downloading the file.
+'''
 @app.post("/detect_video")
 async def detect_video(video: UploadFile=File(...)):
     # Receiving and decoding the image
-    # contents = await video.read()
      # Save the video file locally
     with open("input_video.mp4", "wb") as buffer:
         buffer.write(await video.read())
@@ -78,5 +86,4 @@ async def detect_video(video: UploadFile=File(...)):
     mp4_file_path = labled_video_path
     with open(mp4_file_path, "rb") as f:
         file_content = f.read()
-    # im = cv2.imencode('.png', image_output)[1] # extension depends on which format is sent from Streamlit
     return Response(content=file_content,headers=headers)
